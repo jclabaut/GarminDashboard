0bc48e5fbee72a376bf80382f32c2a2b9c94f8cd
Jérémie Clabaut <jeremie.clabaut@gmail.com>
2026-02-06 17:52:36 +0100

    Initial Commit

---
+++ GarminDashboard/GarminDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarminDashboardApp.swift
+//  GarminDashboard
+//
+//  Created by JEREMIE CLABAUT on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GarminDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
